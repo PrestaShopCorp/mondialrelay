ebc0516c1ed0d014f302508a2bea18d0f693f84d
webincolor <contact@webincolor.fr>
2014-12-23 10:18:03 +0100

    [*] MO : MONDIAL RELAY : Check result before asign variable RelaisPoint, Fix bug Prestashop 1.4 when force load Jquery

--- mondialrelay.php
+++ mondialrelay.php
@@ -72,7 +72,7 @@ class MondialRelay extends Module
 	{
 		$this->name		= 'mondialrelay';
 		$this->tab		= 'shipping_logistics';
-		$this->version	= '2.0.7';
+		$this->version	= '2.0.8';
 		$this->installed_version = '';
 		$this->module_key = '366584e511d311cfaa899fc2d9ec1bd0';
 		$this->author = 'PrestaShop';
@@ -430,7 +430,7 @@ class MondialRelay extends Module
 	public function hookOrderDetailDisplayed($params)
 	{
 		if (!Mondialrelay::isMondialRelayCarrier($params['order']->id_carrier))
-			return '';
+			return;
 
 		$sql = '
 			SELECT s.`MR_Selected_LgAdr1`, s.`MR_Selected_LgAdr2`, s.`MR_Selected_LgAdr3`, s.`MR_Selected_LgAdr4`,
@@ -440,6 +440,9 @@ class MondialRelay extends Module
 			WHERE s.`id_cart` = '.(int)$params['order']->id_cart; 
 		$res = Db::getInstance()->getRow($sql);
 
+		if (!$res)
+			return;
+		
 		$point_relais = $res['MR_Selected_LgAdr1'].
 					($res['MR_Selected_LgAdr1'] ? ' <br/> ' : '').$res['MR_Selected_LgAdr2'].
 					($res['MR_Selected_LgAdr2'] ? ' <br/> ' : '').$res['MR_Selected_LgAdr3'].
@@ -447,9 +450,8 @@ class MondialRelay extends Module
 					($res['MR_Selected_LgAdr4'] ? ' <br/> ' : '').$res['MR_Selected_CP'].' '.
 					$res['MR_Selected_Ville'].' <br/> '.$res['MR_Selected_Pays'];
 			
-		if ((!$res))	
-			return '';
-		elseif (($res['dlv_mode'] == 'LD1') || ($res['dlv_mode'] == 'LDS') || ($res['dlv_mode'] == 'HOM'))
+		
+		if (($res['dlv_mode'] == 'LD1') || ($res['dlv_mode'] == 'LDS') || ($res['dlv_mode'] == 'HOM'))
 			$this->context->smarty->assign(
 				array(
 						'mr_url' => $res['url_suivi']
@@ -562,8 +564,10 @@ class MondialRelay extends Module
 	*/
 	public function hookHeader($params)
 	{
-		$this->context->controller->addJquery();	
-		//Configuration::updateValue('MONDIAL_RELAY_MODE', 'widget');
+		if (version_compare(_PS_VERSION_, '1.5', '<'))
+			$this->context->controller->addJquery();
+		
+		/*Configuration::updateValue('MONDIAL_RELAY_MODE', 'widget');*/
 		if (!($file = basename(Tools::getValue('controller'))))
 			$file = str_replace('.php', '', basename($_SERVER['SCRIPT_NAME']));
 

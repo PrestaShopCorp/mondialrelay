53259f3c730ad77f366ffff16dbb2d10b1929db1
webincolor <contact@webincolor.fr>
2014-12-23 12:05:45 +0100

    [*] MO : MONDIAL RELAY : modify condition just for Prestashop version 1.5 and 1.6

--- mondialrelay.php
+++ mondialrelay.php
@@ -564,7 +564,7 @@ class MondialRelay extends Module
 	*/
 	public function hookHeader($params)
 	{
-		if (version_compare(_PS_VERSION_, '1.5', '<'))
+		if (version_compare(_PS_VERSION_, '1.5', '>='))
 			$this->context->controller->addJquery();
 		
 		/*Configuration::updateValue('MONDIAL_RELAY_MODE', 'widget');*/
